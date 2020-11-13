29d34867a844f562a73b98de9038b8d906c221ef
Ajeet <ajeet_athwal@hotmail.co.uk>
2020-11-13 22:10:37 +0000

    refined button functionality and added random visual

--- myscript.js
+++ myscript.js
@@ -8,32 +8,37 @@ for (let gridNumber = 0; gridNumber < totalGrids; gridNumber++) {
     document.querySelector(".grid").appendChild(cell);
 }
 
-function colorCell(e) {
-    console.log(this.dataset.color);
-    this.style.backgroundColor = "black";
+function colorInCell(e) {
+    if (this.dataset.color === "black") this.style.backgroundColor = "black";
+    if (this.dataset.color === "random") {
+        let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+        this.style.backgroundColor = randomColor;
+    }
+    if (this.dataset.color === "gray-scale") this.style.backgroundColor = "black";
 }
 
 const gridCells = document.querySelectorAll(".grid-cell");
-gridCells.forEach(cell => cell.addEventListener("mouseover", colorCell));
+gridCells.forEach(cell => cell.addEventListener("mouseover", colorInCell));
 
 function clearGridCells() {
     const gridCells = document.querySelectorAll(".grid-cell");
     gridCells.forEach(cell => cell.remove());
 }
 
-function addGridCells(totalGrids) {
+function addGridCells(totalGrids, gridColor) {
     for (let gridNumber = 0; gridNumber < totalGrids; gridNumber++) {
         let cell = document.createElement("div");
         cell.classList.add("grid-cell");
+        cell.setAttribute("data-color", gridColor);
         document.querySelector(".grid").appendChild(cell);
     }
 }
 
-function updateGridCells(totalRows) {
+function updateGridCells(totalRows, gridColor) {
     totalGrids = totalRows * totalRows;
     clearGridCells();
     document.documentElement.style.setProperty("--row-number", totalRows);
-    addGridCells(totalGrids);
+    addGridCells(totalGrids, gridColor);
 
     const gridCells = document.querySelectorAll(".grid-cell");
     gridCells.forEach(cell => cell.addEventListener("mouseover", colorInCell));
@@ -41,13 +46,16 @@ function updateGridCells(totalRows) {
 
 function resetGrid(e) {
     const gridCells = document.querySelectorAll(".grid-cell");
+
+    gridColor = gridCells[0].dataset.color;
     gridCells.forEach(cell => cell.removeAttribute("style"));
+
     totalRows = Number(prompt("How many grid rows would you like? (16 to 100)"));
     if (totalRows !== Number(totalRows)) totalRows = 16;
     if (totalRows < 16) totalRows = 16;
     if (totalRows > 100) totalRows = 100;
-    console.log(totalRows);
-    updateGridCells(totalRows);
+
+    updateGridCells(totalRows, gridColor);
 }
 
 const resetBtn = document.querySelector(".reset-btn");
